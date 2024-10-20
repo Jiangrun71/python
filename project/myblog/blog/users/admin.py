bb355a2102e99dfa9ec6445abc390bcd063475a8
zhangjiang <m17681567973@163.com>
2024-10-20 21:00:01 +0800

    调整目录结构

--- project/myblog/blog/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# 注册模型到Django管理系统中
-
-
--- project/myblog/blog/users/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# 单元测试的文件，用于测试代码的正确性
